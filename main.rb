3cb07b7ef2f0b93ca1596550efb8a01e87d79dab
ongaeshi <ongaeshi0621@gmail.com>
2023-06-24 01:12:07 +0900

    feat: First implimtion

--- main.rb
+++ main.rb
@@ -1,3 +1,23 @@
-require "replacer"
+require "clipboard"
+require "rainbow"
 
-puts "Hello, world!"
+is_second = false
+
+loop do
+  text = Clipboard.paste.encode("UTF-8")
+  unless is_second
+    print "text> "
+    puts text
+    is_second = true
+  end
+
+  print "src> "
+  src = gets.chomp
+
+  print "dst> "
+  dst = gets.chomp
+
+  converted_text = text.gsub(src, dst)
+  puts converted_text.gsub(dst, Rainbow(dst).bg(:red))
+  Clipboard.copy(converted_text)
+end
