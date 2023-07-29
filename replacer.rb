95ca17bd8ff6b0586a93b0a7db3d0326ef17f085
ongaeshi <ongaeshi0621@gmail.com>
2023-07-29 15:16:32 +0900

    refactror: Extract main, replace

--- replacer.rb
+++ replacer.rb
@@ -5,39 +5,44 @@ require "io/console"
 require "yaml"
 require "optparse"
 
-params = {}
-opt = OptionParser.new
-opt.on("-e", "--edit")
-opt.parse!(ARGV, into: params)
+def replace(replacement_list)
+  text = Clipboard.paste.encode("UTF-8")
+  color_text = text
 
-replacer_yml = File.join(File.dirname(__FILE__), "replacer.yml")
+  replacement_list.each do |src, dst|
+    text = text.gsub(src, dst)
+    color_text = color_text.gsub(src, Rainbow(dst).bg(:red))
+  end
 
-if params[:edit]
-  `code #{replacer_yml}`
-  puts "Open setting file '#{replacer_yml}'"
-  exit(0)
-end
+  puts color_text
+  puts
+  puts "Copy to clipboard? (YES ... ENTER/y, NO ... ESC/n)"
 
-# main
-REPLACEMENT_LIST = YAML.load(File.read(replacer_yml))
-text = Clipboard.paste.encode("UTF-8")
-color_text = text
-
-REPLACEMENT_LIST.each do |src, dst|
-  text = text.gsub(src, dst)
-  color_text = color_text.gsub(src, Rainbow(dst).bg(:red))
-end
-
-puts color_text
-puts
-puts "Copy to clipboard? (YES ... ENTER/y, NO ... ESC/n)"
-
-while (key = $stdin.getch) != "\C-c"
-  case key
-  when "y", "\r", "\n"
-    Clipboard.copy(text)
-    break
-  when "n", "\e"
-    break
+  while (key = $stdin.getch) != "\C-c"
+    case key
+    when "y", "\r", "\n"
+      Clipboard.copy(text)
+      break
+    when "n", "\e"
+      break
+    end
   end
 end
+
+def main
+  params = {}
+  opt = OptionParser.new
+  opt.on("-e", "--edit")
+  opt.parse!(ARGV, into: params)
+
+  replacer_yml = File.join(File.dirname(__FILE__), "replacer.yml")
+
+  if params[:edit]
+    `code #{replacer_yml}`
+    puts "Open setting file '#{replacer_yml}'"
+  else
+    replace(YAML.load(File.read(replacer_yml)))
+  end
+end
+
+main
